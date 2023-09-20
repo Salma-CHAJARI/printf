806b600e5cc2f7eb03e884b68fbe8e57d4dc2f46
Majidied <majidimajidi2003@gmail.com>
2023-09-19 17:56:29 -0700

    updated

--- print_hexa.c
+++ print_hexa.c
@@ -1,45 +1,64 @@
 #include "main.h"
+/**
+ * print_hexa_upper - Prints an unsigned number in upper hexadecimal notation
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	return (print_hexa(types, "0123456789ABCDEF", buffer,
+		flags, 'X', width, precision, size));
+}
 
 /**
- * print_hexa - function to print a hexadecimal integer.
- * @types: Variable of va_list containing the hexadecimal integer to be printed
- * @map_to: An array containing custom character mapping ("0123456789ABCDEF").
- * @buffer: The buffer where the output will be stored.
- * @flags: Flags that modify the output (F_MINUS, F_PLUS, F_ZERO).
- * @flag_ch: The character representing the hexadecimal prefix ('x', 'X', 'p').
- * @width: Minimum width of the output (for padding).
- * @precision: Precision of the output (not used for hexadecimal integer).
- * @size: The size of the integer (S_LONG, S_SHORT).
- *
- * Return: Number of chars printed.
+ * print_hexa - Prints a hexadecimal number in lower or upper
+ * @types: Lista of arguments
+ * @map_to: Array of values to map the number to
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @flag_ch: Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * @size: Size specification
+ * Return: Number of chars printed
  */
 int print_hexa(va_list types, char map_to[], char buffer[],
-		int flags, char flag_ch, int width, int precision, int size)
+	int flags, char flag_ch, int width, int precision, int size)
 {
-	unsigned long int a;
-	int r = write_num(0, n, buffer, flags, width, precision, _strlen(map_to));
-	int map_len = _strlen(map_to);
-	char original_char = buffer[i];
-	int i;
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
+	unsigned long int init_num = num;
 
-	if (size == S_LONG)
-		a = va_arg(types, unsigned long int);
-	else if (size == S_SHORT)
-		a = (unsigned short)va_arg(types, unsigned int);
-	else
-		a = va_arg(types, unsigned int);
+	UNUSED(width);
 
-	if (r > 0 && flag_ch != 0)
-		r += handle_write_char(flag_ch, buffer, 0, 0, 0, 0);
+	num = convert_size_unsgnd(num, size);
 
-	if (r > 0)
+	if (num == 0)
+		buffer[i--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
 	{
-		for (i = r - 1; i >= 0; i--)
-		{
-			if (original_char >= 0 && original_char < map_len)
-			buffer[i] = map_to[original_char];
-		}
+		buffer[i--] = map_to[num % 16];
+		num /= 16;
 	}
 
-	return (r);
+	if (flags & F_HASH && init_num != 0)
+	{
+		buffer[i--] = flag_ch;
+		buffer[i--] = '0';
+	}
+
+	i++;
+
+	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
+
